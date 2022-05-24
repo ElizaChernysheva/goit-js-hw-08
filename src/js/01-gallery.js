f19f6e4170d055f7e25558ebdfd10ff5a2e5534d
Liza Chernysheva <lizzychernysheva@gmail.com>
2022-05-24 23:27:52 +0300

    fixes in gallery-js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,19 +1,10 @@
-// Add imports above this line
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 
-
 const galleryEl = document.querySelector(".gallery");
 
 createGallery();
-
-galleryEl.addEventListener("click", (event) => {
-  event.preventDefault();
-  const galleryItemEl = event.target;
-
-  if (galleryItemEl.nodeName !== "A") return;
-});
 
 let lightbox = new SimpleLightbox(".gallery a", {
   overlay: true,
@@ -25,9 +16,11 @@ function createGallery() {
   const galleryItemsEl = galleryItems
     .map(({ preview, original, description }) => {
       return `
+      <li>
         <a class="gallery__item" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
+          <img class="gallery__image" src="${preview}" alt="${description}" />
         </a>
+      </li>
         `;
     })
     .join("");
